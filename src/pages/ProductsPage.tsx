47df4dd7b544de26620a1a454dd0f688b2a6e97f
rdin111 <168043983+rdin111@users.noreply.github.com>
2025-07-21 13:43:30 -0400

    flexbox

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -76,7 +76,7 @@ export function ProductsPage() {
             </div>
 
             {data.content.length > 0 ? (
-                <div className="grid grid-cols-1 md-grid-cols-2 lg-grid-cols-3 gap-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {data.content.map((product: any) => (
                         <ProductCard key={product.id} product={product} />
                     ))}
